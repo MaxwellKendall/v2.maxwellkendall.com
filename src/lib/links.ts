af7c2a7b9f285c9a8b9887c8059cc92f9c92b066
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2024-12-26 12:22:28 -0500

    [resume-link] update to subdomain

--- src/lib/links.ts
+++ src/lib/links.ts
@@ -2,7 +2,6 @@ export const links = {
   github: 'https://github.com/MaxwellKendall',
   linkedin: 'https://www.linkedin.com/in/maxwellnkendall/',
   email: 'mailto:maxwell.n.kendall@gmail.com',
-  resume:
-    'https://maxwell-kendall-resume.s3.amazonaws.com/MAXWELL_KENDALL_Resume.PDF',
+  resume: 'https://resume.maxwellkendall.com',
   linktree: 'https://linktr.ee/maxwellkendall',
 };
